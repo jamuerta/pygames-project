f8786566371f8a8269c5a309c55e7ab889dbe346
javiera <j.mansilla06@ufromail.cl>
2024-06-23 00:25:34 -0400

    mas objetos :c

--- game.py
+++ game.py
@@ -83,7 +83,7 @@ def main(ventana):
     level4 = Level(jugador, "Brown.png")
     level5 = Level(jugador, "Purple.png")
 
-    current_level = level1
+    current_level = level5
 
     level1.load_game_data(objetos_nivel1)
     level2.load_game_data(objetos_nivel2)
@@ -133,6 +133,11 @@ def main(ventana):
                     current_level = level4
                 elif current_level == level4:
                     current_level = level5
+                elif current_level == level5:
+                    game_over(ventana, "Completaste el juego! Felicidades!", WIDTH // 3, HEIGHT // 2)
+                    pygame.display.update()
+                    pygame.time.delay(3000)
+                    pygame.quit()
 
                 jugador.rect.left = 0  
                 jugador.rect.y = 100
@@ -166,15 +171,6 @@ n1_fire1 = Fuego(1000, 250, 16, 32)
 n1_fire2 = Fuego(100, HEIGHT - block_size - 64, 16, 32)
 n1_fire3 = Fuego(355, 152, 16, 32)
 
-n1_spikes1 = Spikes(1100, 487, 28, 28)
-n1_spikes2 = Spikes(1120, 487, 28, 28)
-n1_spikes3 = Spikes(1140, 487, 28, 28)
-
-n1_spikedball1 = Spiked_Ball(595, 440, 28, 28)
-n1_spikedball2 = Spiked_Ball(1300, 152, 28, 28)
-n1_spikedball3 = Spiked_Ball(1455, 56, 28, 28)
-n1_spikedball3 = Spiked_Ball(1770, 344, 28, 28)
-
 n1_fruit1 = speed_fruit(770, 250, 32, 32, "Cherries")
 
 floor_1 = [Bloque(i * block_size, HEIGHT - block_size, block_size, 272,64)
@@ -191,8 +187,11 @@ objetos_nivel1 = [*floor_1, Bloque(0, HEIGHT - block_size * 2, block_size, 272, 
                 Bloque(block_size * 16, HEIGHT - block_size * 5, block_size, 272, 64),
                 Bloque(block_size * 18, HEIGHT - block_size * 2, block_size, 272, 64),
                 Bloque(block_size * 19, HEIGHT - block_size * 2, block_size, 272, 64),
-                    n1_spikes1, n1_spikes2, n1_spikes3, n1_fruit1, n1_fire2, n1_fire1,
-                    n1_spikedball3, n1_fire3, n1_spikedball1, n1_spikedball2]
+                 Spikes(1100, 475, 28, 28), Spikes(1120, 475, 28, 28), Spikes(1140, 475, 28, 28),
+                 Spikes(1410, 92, 28, 28), Spikes(1505, 92, 28, 28),
+                 Spiked_Ball(595, 440, 40, 40), Spiked_Ball(1300, 152, 40, 40), Spiked_Ball(1415, 300, 40, 40),
+                 Spiked_Ball(1770, 344, 40, 40),
+                 n1_fruit1, n1_fire2, n1_fire1, n1_fire3]
 
 ################################################################
 
@@ -204,18 +203,6 @@ n2_fire3 = Fuego(640, 440, 16, 32)
 n2_fire7 = Fuego(1715, 440, 16, 32)
 n2_fire10 = Fuego(1950, 440, 16, 32)
 
-n2_spike1 = Spikes(870, 295, 28, 28)
-n2_spike2 = Spikes(890, 295, 28, 28)
-n2_spike8 = Spikes(910, 295, 28, 28)
-n2_spike9 = Spikes(930, 295, 28, 28)
-n2_spike10 = Spikes(950, 295, 28, 28)
-n2_spike3 = Spikes(1460, 487, 28, 28)
-n2_spike4 = Spikes(1440, 487, 28, 28)
-n2_spike5 = Spikes(1480, 487, 28, 28)
-n2_spike6 = Spikes(1500, 487, 28, 28)
-n2_spike7 = Spikes(1520, 487, 28, 28)
-
-n2_spikedball1 = Spiked_Ball(1760, 260, 28, 28)
 
 objetos_nivel2 = [*floor_2, 
                   Bloque(block_size * 2, HEIGHT - block_size * 2, block_size, 96, 128),
@@ -228,25 +215,16 @@ objetos_nivel2 = [*floor_2,
                   Bloque(block_size * 14, HEIGHT - block_size * 5, block_size, 96, 128),
                   Bloque(block_size * 16, HEIGHT - block_size * 5, block_size, 96, 128),
                   Bloque(block_size * 18, HEIGHT - block_size * 3, block_size, 96, 128),
-                  n2_fire1, n2_fire2, n2_fire3, n2_spike1, n2_spike2, n2_spike3, n2_fire7, 
-                  n2_spike4, n2_fire10, n2_spike5, n2_spike6, n2_spike7, n2_spike8, n2_spike9, 
-                  n2_spikedball1, n2_spike10]
+                  Spikes(870, 285, 28, 28), Spikes(890, 285, 28, 28), Spikes(910, 285, 28, 28),
+                  Spikes(930, 285, 28, 28), Spikes(950, 285, 28, 28), Spikes(1460, 475, 28, 28),
+                  Spikes(1440, 475, 28, 28), Spikes(1480, 475, 28, 28), Spikes(1500, 475, 28, 28),
+                  Spikes(1520, 475, 28, 28), Spiked_Ball(1760, 260, 40, 40),
+                  n2_fire1, n2_fire2, n2_fire3, n2_fire7, n2_fire10]
 
 ################################################################
 
 n3_fire1 = Fuego(150, HEIGHT - block_size - 64, 16, 32)
 n3_fire2 = Fuego(1710, 440, 16, 32)
-
-
-n3_spike1 = Spikes(430, 475, 28, 28)
-n3_spike2 = Spikes(460, 475, 28, 28)
-n3_spike3 = Spikes(490, 475, 28, 28)
-n3_spike4 = Spikes(1320, 475, 28, 28)
-n3_spike5 = Spikes(1350, 475, 28, 28)
-
-n3_spikedball1 = Spiked_Ball(830, 330, 40, 40)
-n3_spikedball2 = Spiked_Ball(1400, 230, 40, 40)
-
 
 
 floor_3 = [Bloque(i * block_size, HEIGHT - block_size, block_size, 0, 0)
@@ -281,9 +259,11 @@ objetos_nivel3 = [*floor_3,
                   Bloque(block_size * 19, HEIGHT - block_size * 4, block_size, 0, 0),
                   Bloque(block_size * 19, HEIGHT - block_size * 5, block_size, 0, 0),
                   Bloque(block_size * 19, HEIGHT - block_size * 6, block_size, 0, 0),
-                  Bloque(block_size * 19, HEIGHT - block_size * 7, block_size, 0, 0), 
-                  n3_spike1, n3_fire1, n3_fire2, n3_spike2, n3_spike3, n3_spike4, n3_spike5, 
-                  n3_spikedball1, n3_spikedball2]
+                  Bloque(block_size * 19, HEIGHT - block_size * 7, block_size, 0, 0),
+                  Spikes(430, 475, 28, 28), Spikes(460, 475, 28, 28), Spikes(490, 475, 28, 28),
+                  Spikes(1320, 475, 28, 28), Spikes(1350, 475, 28, 28), Spiked_Ball(830, 330, 40, 40),
+                  Spiked_Ball(1400, 230, 40, 40),
+                  n3_fire1, n3_fire2]
 
 ################################################################
 
@@ -291,16 +271,6 @@ n4_fire1 = Fuego(370, HEIGHT - block_size - 64, 16, 32)
 n4_fire2 = Fuego(1465, 248, 16, 32)
 n4_fire3 = Fuego(1885, HEIGHT -block_size - 64, 16, 32)
 
-n4_spike1 = Spikes(1250, 475, 28, 28)
-n4_spike2 = Spikes(1280, 475, 28, 28)
-n4_spike3 = Spikes(1310, 475, 28, 28)
-n4_spike4 = Spikes(770, 285, 28, 28)
-n4_spike5 = Spikes(1040, 92, 28, 28)
-n4_spike6 = Spikes(1130, 92, 28, 28)
-n4_spike7 = Spikes(1700, 476, 28, 28)
-
-n4_spikedball1 = Spiked_Ball(360, 200, 40, 40)
-n4_spikedball2 = Spiked_Ball(1740, 50, 40, 40)
 
 floor_4 = [Bloque(i * block_size, HEIGHT - block_size, block_size, 96, 64)
            for i in range(0, 2000)]
@@ -319,8 +289,10 @@ objetos_nivel4 = [*floor_4,
                   Bloque(block_size * 16, HEIGHT - block_size * 3, block_size, 96, 64),
                   Bloque(block_size * 17, HEIGHT - block_size * 3, block_size, 96, 64),
                   Bloque(block_size * 18, HEIGHT - block_size * 4, block_size, 96, 64),
-                  n4_fire1, n4_fire2, n4_fire3, n4_spike1, n4_spike2, n4_spike3, n4_spike4, n4_spike5, n4_spike6,
-                  n4_spikedball2, n4_spikedball1, n4_spike7]
+                  Spikes(1250, 475, 28, 28), Spikes(1280, 475, 28, 28), Spikes(1310, 475, 28, 28),
+                  Spikes(770, 285, 28, 28), Spikes(1040, 92, 28, 28), Spikes(1130, 92, 28, 28),
+                  Spikes(1700, 476, 28, 28), Spiked_Ball(360, 200, 40, 40), Spiked_Ball(1740, 50, 40, 40),
+                  n4_fire1, n4_fire2, n4_fire3]
 
 ##############################################################
 
@@ -328,18 +300,6 @@ n5_fire1 = Fuego(250, 344, 16, 32)
 n5_fire2 = Fuego(1310, 248, 16, 32)
 n5_fire3 = Fuego(1935, 440, 16, 32)
 
-n5_spike1 = Spikes(630, 476, 28, 28)
-n5_spike2 = Spikes(660, 476, 28, 28)
-n5_spike3 = Spikes(310, 476, 28, 28)
-n5_spike4 = Spikes(900, 92, 28, 28)
-n5_spike5 = Spikes(930, 92, 28, 28)
-n5_spike6 = Spikes(1140, 476, 28, 28)
-n5_spike7 = Spikes(1110, 476, 28, 28)
-n5_spike8 = Spikes(6700, 380, 28, 28)
-
-n5_spikedball1 = Spiked_Ball(650, 150, 40, 40)
-n5_spikedball2 = Spiked_Ball(840, 320, 40, 40)
-n5_spikedball3 = Spiked_Ball(1500, 250, 40, 40)
 
 n5_fruit1 = speed_fruit(450, 152, 32, 32, "Bananas")
 
@@ -348,7 +308,7 @@ n5_fruit1 = speed_fruit(450, 152, 32, 32, "Bananas")
 floor_5 = [Bloque(i * block_size, HEIGHT - block_size, block_size, 0, 64)
            for i in range(0, 2000)]
 
-objetos_nivel5 = [*floor_5, n5_fire3, n5_fruit1, n5_spike8,
+objetos_nivel5 = [*floor_5, n5_fire3, n5_fruit1,
                   Bloque(block_size, HEIGHT - block_size * 2, block_size, 0, 64),
                   Bloque(block_size * 2, HEIGHT - block_size * 2, block_size, 0, 64), 
                   Bloque(block_size * 4, HEIGHT - block_size * 4, block_size, 0, 64), 
@@ -360,8 +320,11 @@ objetos_nivel5 = [*floor_5, n5_fire3, n5_fruit1, n5_spike8,
                   Bloque(block_size * 17, HEIGHT - block_size * 2, block_size, 0, 64),
                   Bloque(block_size * 18, HEIGHT - block_size * 2, block_size, 0, 64),
                   Bloque(block_size * 18, HEIGHT - block_size * 3, block_size, 0, 64),
-                  n5_fire1, n5_spike1, n5_spike2, n5_spike3, n5_spike4, n5_spike5, n5_spike6,
-                  n5_spikedball1, n5_spike7, n5_fire2, n5_spikedball2, n5_spikedball3]
+                  Spikes(630, 476, 28, 28), Spikes(660, 476, 28, 28), Spikes(310, 476, 28, 28),
+                  Spikes(900, 92, 28, 28), Spikes(930, 92, 28, 28), Spikes(1140, 476, 28, 28),
+                  Spikes(1110, 476, 28, 28), Spikes(6700, 380, 28, 28), Spiked_Ball(650, 150, 40, 40),
+                  Spiked_Ball(840, 320, 40, 40), Spiked_Ball(1500, 250, 40, 40),
+                  n5_fire1, n5_fire2]
 
 if __name__ == "__main__":
     main(ventana)
